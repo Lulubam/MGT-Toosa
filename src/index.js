dbb6cfe6b137128b7f1c52ae76049f7a797986e3
Lulubam <160643605+Lulubam@users.noreply.github.com>
2025-08-11 15:13:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/Toosan.css';
+import './styles/ToosanApp.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
