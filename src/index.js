a3452aa466b6ecc3aaf507d163b10ee824630db3
Lulubam <160643605+Lulubam@users.noreply.github.com>
2025-08-11 14:41:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CardGame from './CardGame';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
