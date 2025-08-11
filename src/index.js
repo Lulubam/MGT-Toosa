fc368173405d8a80beb187594d94d2dad36424eb
Lulubam <160643605+Lulubam@users.noreply.github.com>
2025-08-11 14:34:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import CardGame from './CardGame';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <CardGame />
+  </React.StrictMode>
+);
